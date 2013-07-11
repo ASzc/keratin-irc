4283d886d6bb8b7e441f265b4e9ccd58a23cc1db
Alex Szczuczko <alex@szc.ca>
2013-07-11 17:42:33 -0400

    Allow more than just TRACE logging levels

--- keratin-bot/src/ca/szc/keratin/bot/misc/Logging.java
+++ keratin-bot/src/ca/szc/keratin/bot/misc/Logging.java
@@ -9,16 +9,25 @@ package ca.szc.keratin.bot.misc;
 import org.pmw.tinylog.Configurator;
 import org.pmw.tinylog.LoggingLevel;
 
+/**
+ * Set tinylog's logging level and output format.
+ */
 public class Logging
 {
-
     /**
-     * Set tinylog's logging level and output format.
+     * Configure at the given logging level
      */
-    public static void activateDefaultLoggingConfig()
+    public static void activateLoggingConfig( LoggingLevel logLevel )
     {
         String formatPattern = "{date:yyyy-MM-dd HH:mm:ss.SSS} [{thread}] {class}.{method}()  {level}: {message}";
-        Configurator.defaultConfig().level( LoggingLevel.TRACE ).formatPattern( formatPattern ).activate();
+        Configurator.defaultConfig().level( logLevel ).formatPattern( formatPattern ).activate();
     }
 
+    /**
+     * Configure at TRACE logging level
+     */
+    public static void activateDebugLoggingConfig()
+    {
+        activateLoggingConfig( LoggingLevel.TRACE );
+    }
 }
